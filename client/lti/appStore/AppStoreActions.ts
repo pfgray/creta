45b0310fe844e45987d1edee202e64a2964fa1de
Paul Gray <pfbgray@gmail.com>
2016-11-20 11:57:14 -0500

    Adding app store builder helpers

--- client/lti/appStore/AppStoreActions.ts
+++ client/lti/appStore/AppStoreActions.ts
@@ -8,15 +8,30 @@ const appStoreService = new AppStoreService();
 export interface FetchAppStoreAction extends Action {
   type: 'FetchAppStoreAction'
 }
+export function FetchAppStoreAction() {
+  return { type: 'FetchAppStoreAction' };
+}
 
 export interface ReceiveAppStoreAction extends Action {
   type: 'ReceiveAppStoreAction'
   appStore: App[]
 }
+export function ReceiveAppStoreAction(appStore: App[]): ReceiveAppStoreAction {
+  return {
+    type: 'ReceiveAppStoreAction',
+    appStore: appStore
+  };
+}
 
 export interface UpdateSearchTextAction extends Action {
   type: 'UpdateSearchTextAction'
   searchText: string
+}
+export function UpdateSearchTextAction(searchText: string): UpdateSearchTextAction {
+  return {
+    type: 'UpdateSearchTextAction',
+    searchText
+  };
 }
 
 export type AppStoreAction = FetchAppStoreAction | ReceiveAppStoreAction | UpdateSearchTextAction;
@@ -25,11 +40,8 @@ export function fetchStorefront(id: string): (d: any) => void {
   //parentheses are required for typescript here to wrap the returning object.
   return dispatch => {
       appStoreService.getAppStore(id)
-        .then(apps => dispatch({
-          type: 'ReceiveAppStoreAction',
-          appStore: apps
-        }))
+        .then(apps => dispatch(ReceiveAppStoreAction(apps)))
         .catch(console.error);
-      dispatch({ type: 'FetchAppStoreAction' });
+      dispatch(FetchAppStoreAction());
     };
 }
